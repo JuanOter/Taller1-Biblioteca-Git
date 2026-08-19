50b60795b3dd455559576437905e25add3fe1531
Juan Manuel Otero Rueda <jmotero@unicesar.edu.co>
2026-08-19 10:59:21 -0500

    refactor: renombrar clase principal a Main

--- src/main/java/com/mycompany/biblioteca/Main.java
+++ src/main/java/com/mycompany/biblioteca/Main.java
@@ -8,7 +8,7 @@ package com.mycompany.biblioteca;
  *
  * @author Hogar
  */
-public class Taller1BibliotecaGit {
+public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
