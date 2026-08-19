53b20ed666d558b3dea0c43eebcd5daa0d702709
Juan Manuel Otero Rueda <jmotero@unicesar.edu.co>
2026-08-19 11:27:06 -0500

    feat: crear clase Prestamo que relaciona Cliente y Libro

--- src/main/java/com/mycompany/biblioteca/Material.java
+++ src/main/java/com/mycompany/biblioteca/Material.java
@@ -1,13 +1,25 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.mycompany.biblioteca;
 
-/**
- *
- * @author Hogar
- */
 public class Material {
-    
+    protected String codigo;
+    protected String titulo;
+    protected int anioPublicacion;
+
+    public Material(String codigo, String titulo, int anioPublicacion) {
+        this.codigo = codigo;
+        this.titulo = titulo;
+        this.anioPublicacion = anioPublicacion;
+    }
+
+    public String getCodigo() { return codigo; }
+    public String getTitulo() { return titulo; }
+    public int getAnioPublicacion() { return anioPublicacion; }
+
+    public void setTitulo(String titulo) { this.titulo = titulo; }
+    public void setAnioPublicacion(int anioPublicacion) { this.anioPublicacion = anioPublicacion; }
+
+    @Override
+    public String toString() {
+        return "Código: " + codigo + " | Título: " + titulo + " | Año: " + anioPublicacion;
+    }
 }
